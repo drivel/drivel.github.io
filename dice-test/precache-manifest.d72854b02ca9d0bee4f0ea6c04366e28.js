97364e79b80441f790ec52db62bbc4e83f8db154
drivel <eternaldrivel@gmail.com>
2019-04-08 20:02:32 -0700

    dice-test

---
+++ dice-test/precache-manifest.d72854b02ca9d0bee4f0ea6c04366e28.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f18807855abe0bb4c2da6ec06c42b1ec",
+    "url": "/dice-test/static/media/dice-2.f1880785.svg"
+  },
+  {
+    "revision": "f1ad8b4c7c7ded662752",
+    "url": "/dice-test/static/css/main.d3277b4e.chunk.css"
+  },
+  {
+    "revision": "8c8f804c65c1b9269f1e",
+    "url": "/dice-test/static/js/runtime~main.4c145d43.js"
+  },
+  {
+    "revision": "cfd093a3ff5b588e1ac3",
+    "url": "/dice-test/static/js/2.3c3492b3.chunk.js"
+  },
+  {
+    "revision": "a8443601b723b6366565c3c5439894a9",
+    "url": "/dice-test/static/media/dice-1.a8443601.svg"
+  },
+  {
+    "revision": "f1ad8b4c7c7ded662752",
+    "url": "/dice-test/static/js/main.2c917c20.chunk.js"
+  },
+  {
+    "revision": "4232d23edea85f1fa2157f26923c795b",
+    "url": "/dice-test/static/media/dice-3.4232d23e.svg"
+  },
+  {
+    "revision": "06c2c351cf1b469fe73411c8c08b090e",
+    "url": "/dice-test/static/media/dice-4.06c2c351.svg"
+  },
+  {
+    "revision": "84ccc49588a593c805157175812ac266",
+    "url": "/dice-test/static/media/dice-5.84ccc495.svg"
+  },
+  {
+    "revision": "295ede1aeedd94c7215add25d705575b",
+    "url": "/dice-test/static/media/dice-6.295ede1a.svg"
+  },
+  {
+    "revision": "5d427f21de2d4710bfbe3f2a04baaa7d",
+    "url": "/dice-test/index.html"
+  }
+];
