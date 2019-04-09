aefde880a0f642968da6c9fb8d34f060aa154710
drivel <eternaldrivel@gmail.com>
2019-04-08 20:13:11 -0700

    test-test

---
+++ test-test/precache-manifest.6843d07b48024b7b345a617c27940afa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6274abd423cd6b75cbaf",
+    "url": "/test-test/static/js/runtime~main.65c7f8af.js"
+  },
+  {
+    "revision": "432b9baaa37b59490db1",
+    "url": "/test-test/static/js/main.32fb92be.chunk.js"
+  },
+  {
+    "revision": "3da246a7f5d6c82b76e0",
+    "url": "/test-test/static/js/2.259cea20.chunk.js"
+  },
+  {
+    "revision": "432b9baaa37b59490db1",
+    "url": "/test-test/static/css/main.17baeafc.chunk.css"
+  },
+  {
+    "revision": "529ffdbad54b3972d7ad0497e4956ebc",
+    "url": "/test-test/index.html"
+  }
+];
